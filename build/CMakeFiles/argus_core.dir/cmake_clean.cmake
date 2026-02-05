76fba43e73684ff121ed760e3c6fd1e0f100a565
Thalia Webb <thaliathenerder@gmail.com>
2026-02-05 11:44:56 -0500

    built it

---
+++ build/CMakeFiles/argus_core.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/argus_core.dir/link.d"
+  "CMakeFiles/argus_core.dir/src/main.cpp.o"
+  "CMakeFiles/argus_core.dir/src/main.cpp.o.d"
+  "argus_core"
+  "argus_core.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/argus_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/argus_core.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for argus_core.
+# This may be replaced when dependencies are built.
